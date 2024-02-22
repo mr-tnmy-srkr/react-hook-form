0b1831d7384c0e83b6e5290fdd74760c87a28951
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-02-22 19:24:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import LoginForm from "./forms/LoginForm";
+import RegistrationForm from "./forms/RegistrationForm";
+
+function App() {
+  return (
+    <>
+      <LoginForm />
+      <br />
+      <br />
+      <br />
+      <RegistrationForm />
+    </>
+  );
+}
+
+export default App;
